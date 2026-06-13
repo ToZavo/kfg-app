642239bf7cc1743ca4aef2380f6a9f151e1da580
Tony Zavolta <tonyzavolta@Tonys-MacBook-Air.local>
2026-06-13 17:10:03 -0400

    moved affirmations into seperate module

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const affirmations = require("./affirmations");
 const express = require("express");
 const app = express();
 
@@ -19,19 +20,21 @@ app.get("/api/spad", (req, res) => {
     });
 });
 
-const affirmations = [
-    "Look where your feet are. In spite of your best efforts you've made it this far. Keep Fucking Going!",
-    "You're not defective, you're a human with weird wiring, and unfinished shit to-do.",
-    "Breathe, dipshit. You don't have to solve life. It'll figure itself out.",
-    "Progress counts even when it looks rough and forced.",
-    "Are you having a shit day, or just a rough 5 minutes you're stretching out?",
-    "There's no scoreboard, you're not behind. You're in position, growing, and still in the game.",
-    "Name 5 things right now you see. That's the assignment."
-];
+
 
 app.get("/api/affirmation", (req, res) => {
-    const randomIndex = Math.floor(Math.random() * affirmations.length);
-    res.json({ affirmation: affirmations[randomIndex] });
+    const today = new Date();
+
+    const startOfYear = new Date(today.getFullYear(), 0, 0);
+    const diff = today - startOfYear;
+    const oneDay = 1000 * 60 * 60 * 24;
+    const dayOfYear = Math.floor(diff / oneDay);
+
+    const affirmationIndex = dayOfYear % affirmations.length;
+
+    res.json({
+        affirmation: affirmations[affirmationIndex],
+    });
 });
 
 app.listen(3000, () => {
