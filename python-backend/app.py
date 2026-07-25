27a7f5c26ebaa2f0b65515153f789aa8c83ca452
Tony Zavolta <tonyzavolta@Tonys-MacBook-Air.local>
2026-07-25 03:48:23 -0400

    added save entry to database

--- python-backend/app.py
+++ python-backend/app.py
@@ -1,5 +1,5 @@
-from flask import Flask
-from database import init_db
+from flask import Flask, request, jsonify
+from database import init_db, save_entry
 
 app = Flask(__name__)
 init_db()
@@ -7,6 +7,12 @@ init_db()
 @app.route("/")
 def home():
     return "Backend is running!"
+
+@app.route("/entry", methods=["POST"])
+def add_entry():
+    data = request.get_json()
+    save_entry(data["date"], data["content"], data["clean_days"])
+    return jsonify({"status": "saved"})
 
 if __name__ == "__main__":
     app.run(debug=True)
