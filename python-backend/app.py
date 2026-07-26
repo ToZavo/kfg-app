aa8935a0072f365b03be7b624f79ee2c7b3eeb72
Tony Zavolta <tonyzavolta@Tonys-MacBook-Air.local>
2026-07-25 20:58:19 -0400

    consolidated affirmations

--- python-backend/app.py
+++ python-backend/app.py
@@ -21,5 +21,19 @@ def get_entries():
     entries = get_all_entries()
     return jsonify(entries)
 
+@app.route('/api/affirmation')
+def get_affirmation():
+    affirmations = [
+    "Look where your feet are. In spite of your best efforts you've made it this far. Keep Fucking Going!",
+    "You're not defective, you're a human with weird wiring, and unfinished shit to-do.",
+    "Breathe, dipshit. You don't have to solve life. It'll figure itself out.",
+    "Progress counts even when it looks rough and forced.",
+    "Are you having a shit day, or just a rough 5 minutes you're stretching out?",
+    "There's no scoreboard, you're not behind. You're in position, growing, and still in the game.",
+    "Name 5 things right now you see. That's the assignment."
+    ]
+    import random
+    return jsonify({"affirmation": random.choice(affirmations)})
+
 if __name__ == "__main__":
     app.run(debug=True)
