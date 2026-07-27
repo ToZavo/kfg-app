4e2dbe55f62c3b7b6649fd25ff9b8deefb8580ba
Tony Zavolta <tonyzavolta@mac.mynetworksettings.com>
2026-07-27 10:34:36 -0400

    added JFT and SPaD

--- public/readings.js
+++ public/readings.js
@@ -1,26 +1,46 @@
 const dailyReadings = {
     jft: {
         title: "Just For Today",
-        reading: ` Living clean
-Page 169
+        reading: ` We do recover
+Page 217
 
-"As we recover, we gain a new outlook on being clean...Life can become a new adventure for us."
+"After coming to NA, we found ourselves among a very special group of people who have suffered like us and found recovery. In their experiences, freely shared, we found hope for ourselves. If the program worked for them, it would work for us."
 
-Basic Text, p. 91
+Basic Text, p. 10
 
-The using life is not a clean one--no one knows this better than we do. Some of us lived in physical squalor, caring neither for our surroundings nor ourselves. Worse, though, than any external filth was the way most of us felt inside. The things we did to get our drugs, the way we treated other people, and the way we treated ourselves had us feeling dirty. Many of us recall waking too many mornings just wishing that, for once, we could feel clean about ourselves and our lives.
+A newcomer walks into his or her first meeting, shaking and confused. People are milling about. Refreshments and literature are set out. The meeting starts after everyone has drifted over to their chairs and settled themselves in. After taking a bewildered glance at the odd assortment of folks in the room, the newcomer asks, "Why should I bet my life on this group? After all, they're just a bunch of addicts like me."
 
-Today, we have a chance to feel clean by living clean. For us addicts, living clean starts with not using--after all, that's our primary use for the word "clean" in Narcotics Anonymous. But as we stay "clean" and work the Twelve Steps, we discover another kind of clean. It's the clean that comes from admitting the truth about our addiction rather than hiding or denying our disease. It's the freshness that comes from owning up to our wrongs and making amends for them. It's the vitality that comes from the new set of values we develop as we seek a Higher Power's will for us. When we practice the principles of our program in all our affairs, we have no reason to feel dirty about our lives or our lifestyles--we're living clean, and grateful to be doing so at last.
+Though it may be true that not many of our members had much going for us when we got here, the newcomer soon learns that the way we are living today is what counts. Our meetings are filled with addicts whose lives have turned completely around. Against all odds, we are recovering. The newcomer can relate to where we've been and draw hope from where we are now. Today, every one of us has the opportunity to recover.
 
-"Clean living" used to be just for the "squares." Today, living clean is the only way we'd have it.
+Yes, we can safely entrust our lives to our Higher Power and to Narcotics Anonymous. So long as we work the program, the payoff is certain: freedom from active addiction and a better way of life.
 
-Just for Today: I feel clean because I'm living clean--that's the way I want to keep it.`
+Just for Today: The recovery I've found in Narcotics Anonymous is a sure thing. By basing my life on it, I know I will grow.`
     },
 
     spad: {
         title: "Spiritual Principle a Day",
         date: "06/11/2026",
-        reading: "Paste SPaD Here."
+        reading: `27 July 
+
+Our Step-One Surrender
+
+Page 215 
+
+"Only in working the First Step do we truly come to realize that we are addicts, that we have hit bottom, and that we must surrender."
+
+NA Step Working Guides, Step One, Opening Essay
+
+
+
+We don't all enter the rooms of NA certain that we are addicts like those people. Some of us are dubious. Now that we have a couple of weeks clean, we remember our using days a bit differently: Was it that bad? Do we really have a "disease"? Sure, we have a problem with drugs, but it's not like we were ever arrested for it. We have a roof over our head and teeth in our mouth. Never have we exchanged sex for drugs, and all our student loan payments have been on time. Was our bottom so terrible? Was it terrible enough to warrant a daily surrender? An oldtimer offers some unhelpful advice: "Maybe you aren't done yet." That sounds ominous, and we definitely have some sort of problem, so . . .
+
+We stay clean and get a sponsor. We pick up the NA Step Working Guides and, at our sponsor's direction, begin to answer the questions as honestly and thoroughly as possible. By the time we get to the section on surrender, we've already written about our "disease" at length: our profound dishonesty and denial, our manipulation of the people who loved and trusted us, all the laws we broke (even if we didn't get caught), the powerlessness over our addiction, our obsessiveness, our compulsiveness, our obsessive-compulsiveness, the unmanageability we've created in our lives, and the reservations we may be holding onto.
+
+Seeing it all there on the page, all that proof in black and white--it's undeniable. I am an addict.In an ideal world, that's the moment of surrender we never look back from. Sure, that happens for many of us. That's the beginning of our process of surrendering, opening the door to recovery. Others of us end up getting loaded, doing more "research," hitting a lower bottom, and surrendering later. Still others never make it back. 
+
+
+
+I've already done enough research. What is unmanageable in my life right now? What am I obsessing about? What can I do to surrender today?`
 
     }
 };
